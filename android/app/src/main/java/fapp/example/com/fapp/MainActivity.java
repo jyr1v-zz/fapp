74ca27cd4d887262532218396c17efc7d27d0434
Jyri Vehmaskoski <jyri@jyriv.net>
2018-12-20 15:46:57 +0200

    Initial commit

---
+++ android/app/src/main/java/fapp/example/com/fapp/MainActivity.java
@@ -0,0 +1,13 @@
+package fapp.example.com.fapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
