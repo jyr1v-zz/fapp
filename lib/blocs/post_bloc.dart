3fdc313c3f4c3906f8a87843312c61838b0108c7
Jyri Vehmaskoski <jyri@jyriv.net>
2018-12-31 22:14:18 +0200

    infinite list scrolling

---
+++ lib/blocs/post_bloc.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:http/http.dart' as http;
+import 'package:bloc/bloc.dart';
+
+import 'package:fapp/blocs/bloc.dart';
+import 'package:fapp/models/models.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBloc({@required this.httpClient});
+
+  @override
+  Stream<PostEvent> transform(Stream<PostEvent> events) {
+    return (events as Observable<PostEvent>)
+        .debounce(Duration(milliseconds: 500));
+  }
+
+  @override
+  get initialState => PostState.initial();
+
+  @override
+  Stream<PostState> mapEventToState(currentState, event) async* {
+    if (event is Fetch && !currentState.hasReachedMax) {
+      try {
+        final posts = await _fetchPosts(currentState.posts.length, 20);
+
+        if (posts.isEmpty) {
+          yield currentState.copyWith(hasReachedMax: true);
+        } else {
+          yield PostState.success(currentState.posts + posts);
+        }
+      } catch (_) {
+        yield PostState.failure();
+      }
+    }
+  }
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get(
+        'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit');
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+
+      return data.map((rawPost) {
+        return Post(
+          id: rawPost['id'],
+          title: rawPost['title'],
+          body: rawPost['body'],
+        );
+      }).toList();
+    } else {
+      throw Exception('error fetching posts');
+    }
+  }
+}
---
+++ lib/blocs/post_state.dart
@@ -0,0 +1,77 @@
+import 'package:fapp/models/models.dart';
+
+class PostState {
+  final bool isInitializing;
+  final List<Post> posts;
+  final bool isError;
+  final bool hasReachedMax;
+
+  PostState({
+    this.isError,
+    this.isInitializing,
+    this.posts,
+    this.hasReachedMax,
+  });
+
+  factory PostState.initial() {
+    return PostState(
+      isInitializing: true,
+      posts: [],
+      isError: false,
+      hasReachedMax: false,
+    );
+  }
+
+  factory PostState.success(List<Post> posts) {
+    return PostState(
+      isInitializing: false,
+      posts: posts,
+      isError: false,
+      hasReachedMax: false,
+    );
+  }
+
+  factory PostState.failure() {
+    return PostState(
+      isInitializing: false,
+      posts: [],
+      isError: true,
+      hasReachedMax: false,
+    );
+  }
+
+  PostState copyWith({
+    bool isInitializing,
+    List<Post> posts,
+    bool isError,
+    bool hasReachedMax,
+  }) {
+    return PostState(
+      isInitializing: isInitializing ?? this.isInitializing,
+      posts: posts ?? this.posts,
+      isError: isError ?? this.isError,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+    );
+  }
+
+  @override
+  String toString() =>
+      'PostState { isInitializing: $isInitializing, posts: ${posts.length}, isError: $isError, hasReachedMax: $hasReachedMax }';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PostState &&
+          runtimeType == other.runtimeType &&
+          isInitializing == other.isInitializing &&
+          posts == other.posts &&
+          isError == other.isError &&
+          hasReachedMax == other.hasReachedMax;
+
+  @override
+  int get hashCode =>
+      isInitializing.hashCode ^
+      posts.hashCode ^
+      isError.hashCode ^
+      hasReachedMax.hashCode;
+}
